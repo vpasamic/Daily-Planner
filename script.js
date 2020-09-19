5b4c960a385967dda234d7cc7c22c21ba04eb514
Ver <vpasamic22@gmail.com>
2020-09-18 18:13:45 -0700

    made it so that the page doesn't refresh what's in the textbox every minute by making another loop that will only check it once while the page loads

--- script.js
+++ script.js
@@ -13,51 +13,48 @@ var hour= ["9","10","11","12","13","14","15","16","17"]
 function timecheck(){
     for(var i= 0; i<hour.length; i++){
         time=parseInt(time)
-        x=parseInt(hour[i]);
+        var x=parseInt(hour[i]);
         var id = "hour-"+x
-
-        console.log(id)
-        var hi=localStorage.getItem(id)
-        console.log(hi)
         if (time<x){
             $("#"+id).removeClass("past");
             $("#"+id).removeClass("present");
             $("#"+id).addClass("future");
-            $("#"+id).children(".description").val(hi)
         }
         else if (time>x){
             $("#"+id).addClass("past");
             $("#"+id).removeClass("present");
             $("#"+id).removeClass("future");
-            $("#"+id).children(".description").val(hi)
+         
         }
         else if (time===x){
             $("#"+id).removeClass("past");
             $("#"+id).addClass("present");
             $("#"+id).removeClass("future");
-            $("#"+id).children(".description").val(hi)
+            
         }
     }
+}
+
+for(var i= 0; i<hour.length; i++){
+    var x=parseInt(hour[i])
+    var id = "hour-"+x
+    var hi=localStorage.getItem(id)
+    $("#"+id).children(".description").val(hi)
 }
 
 timecheck()
 setInterval(function (){
     timecheck()
 },1000*60);
+
 // save local storage from text area
 $(".saveBtn").on("click",function(){
-    console.log("hi");
     var todo = $(this).siblings(".description").val();
-    console.log(todo);
     var id=($(this).parent(".row").attr("id"))
-    localStorage.setItem(id,todo)
-    console.log()
-    var hi=localStorage.getItem(id)
-        console.log(hi)
-    console.log(localStorage.getItem("hour-17"))
+    localStorage.setItem(id,todo);
 })
-var hi =localStorage
-console.log(hi)
+
+
 
 
 
